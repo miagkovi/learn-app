39ff4fe9a66a41dd2cd64eb4ef8c3e0baa70d6d2
ievgenii <miagkovi@gmail.com>
2017-02-20 21:12:42 +0200

    add ajax

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,10 +3,18 @@ var app = new Vue({
   data: {
     tasks: this.tasks
   },
+  methods: {
+    createTask: function(){
+      this.$http.post('/tasks', {task: {body: "test6", priority: 10, done: false}})
+        .then(function(res){
+          this.tasks.push(res.body);
+        });
+    }
+  },
   created: function(){
     this.$http.get('/tasks.json')
       .then(function(res){
         this.tasks = res.data;
-    })
+      });
   }
 })
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: ->{request.format.json?}
 
   def spa
     render 'layouts/application'
